e8b8abe8cfc2a0733bd892dfb10274146c8b47c5
ANDRES H CARRERA <andreshcar@live.com>
2019-01-14 15:37:28 -0700

    adding some more features to the application

--- command_line_app.py
+++ command_line_app.py
@@ -1,6 +1,7 @@
 import cyphers
 import console_io as c_io
 import os
+import transposition
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
@@ -17,22 +18,23 @@ def app():
                 running = brute_force_finder()
         else: 
             running = False
-
+def brute_force_finder():
+    user_input = c_io.get_input("Enter message to brute force")    
+    pass
 
 def encryptor_decryptor():
     cypher_names = list(map(cyphers.get_name, cyphers.cyphers))
     running = True
-    menu_options = ["Encrypt", "Decrypt"]
     selection = c_io.menu_selection(cypher_names, True)
     if(selection > 0):
         selection -= 1
-        action = c_io.menu_selection(menu_options)
+        action = c_io.menu_selection(cyphers.actions)
         action -= 1
         user_input = c_io.get_input("Enter a Message", False);
         was_valid_input = False
         while(not was_valid_input):
             try:
-                cyphers.do_action_based_on_cypher(cyphers.cyphers[selection], menu_options[action], message=user_input)
+                cyphers.do_action_based_on_cypher(cyphers.cyphers[selection], cyphers.actions[action], message=user_input)
                 was_valid_input = True
             except AssertionError as error:
                 print(error)
--- cyphers.py
+++ cyphers.py
@@ -12,23 +12,8 @@ cyphers = [reverse, caesar, bacons, transposition, word_trasnposition_cipher]
 actions = ["Encrypt", "Decrypt"] 
 def get_name(obj):
     return obj.name
-
-def validate_inputs(selection, action):
-    if(selection is None):
-        raise AssertionError("selection cannot be set to None.")
-    if(action is None):
-        raise AssertionError("action cannot be None.")
-    if(type(selection) is not int):
-        raise AssertionError("selection must be an integer.")
-    if(type(action) is not int):
-        raise AssertionError("action must be an integer.")
-    if(action > 2 or action < 1):
-        raise AssertionError("action must be between 1 and 2.")
-    if(selection > len(cyphers) or selection < 0):
-        raise AssertionError("selection must be between 0 and the length of the cyphers array")
-
+    
 def do_action_based_on_cypher(selection, action, message=None, key=None):
-    validate_inputs(selection, action)
     if(action in actions[0]):
         parameters = len(inspect.signature(selection.encrypt).parameters)
         encrypted_output = None
